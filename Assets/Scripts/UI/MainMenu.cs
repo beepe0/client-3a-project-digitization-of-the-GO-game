e445a315485e88c54539d060140065f82b48f7d9
Bartek424 <b.slojkowski965@gmail.com>
2023-10-21 17:18:06 +0200

    Dodałem menu v1

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace UI
+{
+    public class MainMenu : MonoBehaviour
+    {
+        public void PlayGame()
+        {
+            SceneManager.LoadScene("test_scene");
+        }
+    }
+}
